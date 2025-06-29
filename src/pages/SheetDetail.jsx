eb9430cc451b2393f3176ee3020aebddbc52dd1f
Alice Ji <axji@wm.edu>
2025-06-28 21:52:18 -0400

    database linked

--- src/pages/SheetDetail.jsx
+++ src/pages/SheetDetail.jsx
@@ -3,6 +3,14 @@ import React from 'react';
 import { useParams, Link } from 'react-router-dom';
 import sheets from '../data/sheets';
 
+const trackDownload = async (pdfId) => {
+  await fetch("https://98o7or2p30.execute-api.us-east-1.amazonaws.com/increment", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ pdfId }),
+  });
+};
+
 function SheetDetail() {
   const { id } = useParams();
   const sheet = sheets.find((s) => s.id === id);
@@ -15,7 +23,7 @@ function SheetDetail() {
     <div className="container">
       <h1>{sheet.title}</h1>
       <p><strong>Composer:</strong> {sheet.composer}</p>
-      <a href={sheet.url} download className="download-link">
+      <a href={sheet.url} onClick={() => trackDownload(sheet.id)} download className="download-link">
         ⬇️ Download PDF
       </a>
       <br />
