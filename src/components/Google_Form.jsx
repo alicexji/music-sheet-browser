0dbe4fc540bd595664e93e18b92a75e300c35ef1
Alice Ji <axji@wm.edu>
2025-09-01 14:06:50 -0400

    upload with google form

---
+++ src/components/Google_Form.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+const GoogleForm = () => {
+  return (
+    <div style={{ width: '100%', height: '100%' }}>
+      {/* <iframe
+        src="https://docs.google.com/forms/d/e/1FAIpQLSeEXAMPLE/viewform?embedded=true"
+        width="100%"
+        height="800"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
+        title="Google Form"
+      >
+        Loading…
+      </iframe> */}
+    <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSeg4hAy7THJEfZR2EibvL5R3kfONDw5WzUuY5S3fD3sJ_n0OA/viewform?embedded=true" 
+    width="640" height="2061" frameborder="0" marginheight="0" marginwidth="0">
+    Loading…</iframe>
+    </div>
+  );
+};
+
+export default GoogleForm;
--- src/pages/Upload.jsx
+++ src/pages/Upload.jsx
@@ -1,10 +1,7 @@
 
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
-import sheets from '../data/sheets';
-import SheetCard from './SheetCard';
-import Header from '../components/Header';
-import Web_Card from '../components/Web_Card';
+import GoogleForm from '../components/Google_Form';
 import '../App.css';
 
 function Upload() {
@@ -13,9 +10,10 @@ function Upload() {
   return (
     <>
       
-      <div style={{marginTop: 120}} className="content-wrapper">
+      <div style={{marginTop: 120, marginBottom: 120}} className="content-wrapper">
         
-        <p>Coming Soon.</p>
+        <p>Upload Music For Review</p>
+        <GoogleForm />
           
       </div>
     </>
